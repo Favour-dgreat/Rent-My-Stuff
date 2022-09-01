ebede75e7b1fe3c423f3c02c2d5373e345dce2f7
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-09-01 10:49:21 +0400

    new features and cleanup

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,32 +1,106 @@
 import { Item, listedItems } from "./model";
-import { ContractPromiseBatch, context } from "near-sdk-as";
+import { ContractPromiseBatch, context, u128 } from "near-sdk-as";
 
 export function setItem(item: Item): void {
-  let storedItem = listedItems.get(item.id);
-  if (storedItem !== null) {
-    throw new Error(`item with ${item.id} already exists`);
-  }
-  listedItems.set(item.id, Item.fromPayload(item));
+	let storedItem = listedItems.get(item.id);
+	if (storedItem !== null) {
+		throw new Error(`item with ${item.id} already exists`);
+	}
+	listedItems.set(item.id, Item.fromPayload(item));
 }
 
 export function getItem(id: string): Item | null {
-  return listedItems.get(id);
+	return listedItems.get(id);
 }
 
 export function getItems(): Item[] {
-  return listedItems.values();
+	return listedItems.values();
 }
 
-export function sellItem(itemId: string): void {
-  const item = getItem(itemId);
-  if (item == null) {
-    throw new Error("item not found");
-  }
-  if (item.owner != context.sender) {
-    throw new Error("You are not the owner");
-  }
-  item.isSale = true;
-  item.isBought = false;
-  listedItems.set(item.id, item);
+export function putOnSale(itemId: string, price: u128): void {
+	const item = getItem(itemId);
+	if (item == null) {
+		throw new Error("item not found");
+	}
+	if (item.owner != context.sender) {
+		throw new Error("You are not the owner");
+	}
+	assert(
+		item.isRented == false,
+		"Status of item can't be changed while being rented"
+	);
+	item.putOnSale();
+	item.changePrices(price, "sale");
+	listedItems.set(item.id, item);
+}
 
+export function putOnRent(itemId: string, price: u128): void {
+	const item = getItem(itemId);
+	if (item == null) {
+		throw new Error("item not found");
+	}
+	if (item.owner != context.sender) {
+		throw new Error("You are not the owner");
+	}
+	assert(
+		item.isRented == false,
+		"Status of item can't be changed while being rented"
+	);
+	item.putOnRent();
+	item.changePrices(price, "rent");
+	listedItems.set(item.id, item);
+}
+
+export function buyItem(itemId: string): void {
+	const item = getItem(itemId);
+	if (item == null) {
+		throw new Error("item not found");
+	}
+	assert(item.isSale == true, "Item isn't for sale");
+	assert(
+		item.owner.toString() != context.sender.toString(),
+		"You can't buy your own item"
+	);
+	assert(
+		item.price.toString() == context.attachedDeposit.toString(),
+		"Attached deposit has to match the price of item"
+	);
+	ContractPromiseBatch.create(item.owner).transfer(context.attachedDeposit);
+	item.buy();
+	listedItems.set(item.id, item);
+}
+
+export function rentItem(itemId: string, duration: u64): void {
+	const item = getItem(itemId);
+	if (item == null) {
+		throw new Error("item not found");
+	}
+	assert(item.isRent == true, "Item isn't for sale");
+	assert(
+		item.owner.toString() != context.sender.toString(),
+		"You can't rent your own item"
+	);
+	assert(
+		item.rentPrice.toString() == context.attachedDeposit.toString(),
+		"Attached deposit has to match the price of item"
+	);
+	assert(duration > u64.MIN_VALUE, "Invalid duration");
+	ContractPromiseBatch.create(item.owner).transfer(context.attachedDeposit);
+	item.rent(duration);
+	listedItems.set(item.id, item);
+}
+
+export function endRentItem(itemId: string): void {
+	const item = getItem(itemId);
+	if (item == null) {
+		throw new Error("item not found");
+	}
+	assert(item.isRented == true, "Item isn't being rented");
+	assert(
+		item.owner.toString() == context.sender.toString() ||
+			item.renter.toString() == context.sender.toString(),
+		"Only owner or renter of item can end rent"
+	);
+  item.endRent();
+	listedItems.set(item.id, item);
 }
--- assembly/model.ts
+++ assembly/model.ts
@@ -2,38 +2,74 @@ import { PersistentUnorderedMap, u128, context } from "near-sdk-as";
 
 @nearBindgen
 export class Item {
-  id: string;
-  name: string;
-  location:string;
-  date:string;
-  description: string;
-  image: string;
-  price: u128;
-  owner: string;
-  renter:string;
-  isRent: bool;
-  isSale: bool;
-  isBought: bool;
-  isRented: bool;
-  isUsed: bool;
-  public static fromPayload(payload: Item): Item {
-    const item = new Item();
-    item.id = payload.id;
-    item.name = payload.name;
-    item.description = payload.description;
-    item.location = payload.location;
-    item.date = payload.date;
-    item.image = payload.image;
-    item.price = payload.price;
-    item.owner = context.sender;
-    item.renter = "";
-    item.isUsed = payload.isUsed;
-    item.isRent = payload.isRent;
-    item.isSale = payload.isSale;
-    item.isBought = false;
-    item.isRented = false;
-    return item;
+	id: string;
+	name: string;
+	location: string;
+	date: string;
+	description: string;
+	image: string;
+	price: u128;
+  rentPrice: u128;
+	owner: string;
+	renter: string;
+  rentTime: u64;
+	isRent: bool;
+	isSale: bool;
+	isRented: bool;
+	public static fromPayload(payload: Item): Item {
+		const item = new Item();
+		item.id = payload.id;
+		item.name = payload.name;
+		item.description = payload.description;
+		item.location = payload.location;
+		item.date = payload.date;
+		item.image = payload.image;
+		item.price = payload.price;
+    item.rentPrice = payload.rentPrice;
+    item.rentTime = u64.MIN_VALUE;
+		item.owner = context.sender;
+		item.renter = "";
+		item.isRent = payload.isRent;
+		item.isSale = payload.isSale;
+		item.isRented = false;
+		return item;
+	}
+
+	public putOnSale(): void {
+		this.isSale = true;
+    this.isRent = false;
   }
+
+  public putOnRent(): void {
+    this.isRent = true;
+    this.isSale = false;
+  }
+
+  public changePrices(price: u128, priceOf: string): void {
+    if(priceOf == "sale"){
+      this.price = price;
+    }else if (priceOf == "rent"){
+      this.rentPrice = price;
+    }else { 
+      return;
+    }
+  }
+
+  public rent(duration :u64): void {
+    this.renter = context.sender;
+    this.rentTime = duration + context.blockTimestamp;
+  }
+
+  public buy() : void {
+    this.isSale = false;
+    this.owner = context.sender;
+  }
+
+  public endRent(): void {
+    this.renter = "";
+    this.rentTime = u64.MIN_VALUE;
+  }
+
 }
 
 export const listedItems = new PersistentUnorderedMap<string, Item>("Items");
